9b7e87255d4a4fc6c573b407bbf4d2a97ac29412
Immortal.S <342333349@qq.com>
2020-01-29 14:46:45 +0800

    2 axis

--- diagram.py
+++ diagram.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 
 
 def smooth(seq):
-    m = 10
+    m = 20
     n = len(seq)
     res = []
     for i in range(n-m):
@@ -24,6 +24,7 @@ def smooth(seq):
 
 
 log_path = '/data/shenzhonghai/FaceClustering/logs/train_log_Vgg16_bs-128_lr-8|12k|15k.log'
+# log_path = '/data/shenzhonghai/FaceClustering/logs/train_log_Vgg16_bs-128_lr-400.log'
 acc = []
 loss = []
 iters = 0
@@ -34,25 +35,29 @@ with open(log_path, 'r') as f:
         if re.search('iters', st) is None:
             continue
         iters += 1
-        loc = re.search(r'loss: [\d]*\.[\d]*', st).span()
-        loss.append(float(st[loc[0]+6:loc[1]]))
         if iters % 104 == 0:
             continue
+        loc = re.search(r'loss: [\d]*\.[\d]*', st).span()
+        loss.append(float(st[loc[0]+6:loc[1]]))
         loc = re.search(r'acc: [\d]*', st).span()
-        acc.append(int(st[loc[0]+5:loc[1]]))
+        acc.append(float(st[loc[0]+5:loc[1]])*100/128)
+        # loc = re.search(r'acc: [\d]*\.[\d]', st).span()
+        # acc.append(float(st[loc[0]+5:loc[1]])*100)
 loss = smooth(loss)
 acc = smooth(acc)
 
 iters = len(acc)
 x = np.linspace(0, iters, iters)
-# plt.plot(x, loss, label='loss')
-# plt.ylim(0., 9.)
-plt.plot(x, acc, label='acc')
-plt.ylim(0., 128.)
+fig, ax1 = plt.subplots()
+ax2 = ax1.twinx()
+ax1.plot(x, loss, label='loss', color='r')
+ax2.plot(x, acc, label='acc', color='b')
+ax1.set_ylim(0., 2.)
+ax2.set_ylim(0., 100.)
+ax1.set_ylabel('loss')
+ax2.set_ylabel('acc')
 plt.xlabel('iterations')
-plt.ylabel('Value')
-# plt.title("Loss Diagram")
-plt.title("Acc Diagram")
-plt.legend()
+plt.title("Loss/Acc Diagram")
+fig.legend(loc=0, bbox_to_anchor=(0.3, 1), bbox_transform=ax1.transAxes)
 
 plt.show()
