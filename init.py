6f08c12d302f9730456a4b130d69f47430b4b389
Immortal.S <342333349@qq.com>
2020-02-04 23:42:10 +0800

    progressBar

--- init.py
+++ init.py
@@ -1,43 +1,48 @@
 import cv2
 import os
+import time
 import torch
 import numpy as np
 # from PIL import Image
 from torch.utils.data import Dataset, DataLoader, TensorDataset
+from progressbar import *
 # person 5749 Total 13233 (lfw)
 
 # filePath = "/data/shenzhonghai/lfw/lfw-deepfunneled"
 filePath = "/data/shenzhonghai/lfw/lfw"
 pathDir = os.listdir(filePath)
-dataset = []
-label = []
-name = []
-person = 0
 print('data path:', filePath)
 
 
 class DataReader(Dataset):
-
+    dataset = []
+    label = []
+    name = []
+    person = 0
+    rng = np.random
+    widgets = ['Data Loading: ', Percentage(), ' ', Bar('#'), ' ', Timer(),
+               ' ', ETA(), ' ', FileTransferSpeed()]
+    pgb = ProgressBar(widgets=widgets, maxval=10 * 5749).start()
     for allDir in pathDir:
         child = os.path.join('%s/%s' % (filePath, allDir))
         childDir = os.listdir(child)
-        name.append(allDir)
         for allSon in childDir:
             son = os.path.join('%s/%s' % (child, allSon))
             dataset.append(cv2.imread(son))
             label.append(person)
+            name.append(son)
+        pgb.update(person * 10 + 1)
         person += 1
-        if person % 500 == 0:
-            print(person)
         # if person == 1000:
         #     break
+    pgb.finish()
+    print('Data Loaded!')
 
     dataset = np.transpose(np.array(dataset, dtype=float), [0, 3, 1, 2])
     label = np.array(label)
     print('data:', dataset.shape)
     print('label:', label.shape)
-    print(label[:5])
-    rng = np.random
+    print('label_value:', label[:5])
 
     def __init__(self):
         self.x = torch.FloatTensor(self.dataset)
