cb8c8d6039d5ab4e9074841d54ac3954a1eac595
Immortal.S <342333349@qq.com>
2020-02-04 20:26:42 +0800

    acc:x.y

--- diagram.py
+++ diagram.py
@@ -2,17 +2,9 @@ import re
 import numpy as np
 import matplotlib.pyplot as plt
 
-# st = 'epoch: 159/160, iters: 16640, lr: 0.000001, loss: 0.37112, acc: 46, train_time: 0.0519, test_time: 0.00015, ' \
-#      'data_time: 0.0029 '
-# print(re.search('z', st) is None)
-# print(re.search(r'loss: [\d]*\.[\d]*, acc', st).span())
-# loc = re.search(r'loss: [\d]*\.[\d]*, acc', st).span()
-# print(float(st[loc[0]+6:loc[1]-5]))
-# exit(0)
-
 
 def smooth(seq):
-    m = 20
+    m = 10
     n = len(seq)
     res = []
     for i in range(n-m):
@@ -24,13 +16,11 @@ def smooth(seq):
 
 
 # log_path = '/data/shenzhonghai/FaceClustering/logs/train_log_Vgg16_bs-128_lr-8|12k|15k.log'
-log_path = '/data/shenzhonghai/FaceClustering/logs/train_log_Vgg16_bs-128_lr-400.log'
+log_path = '/data/shenzhonghai/FaceClustering/logs/train_log_Vgg16_bs-128_lr-4|16k|19k.log'
 acc = []
 loss = []
 iters = 0
 with open(log_path, 'r') as f:
-    # print(f.read())
-    # exit(0)
     for st in f.readlines():
         if re.search('iters', st) is None:
             continue
@@ -42,7 +32,7 @@ with open(log_path, 'r') as f:
         # loc = re.search(r'acc: [\d]*', st).span()
         # acc.append(float(st[loc[0]+5:loc[1]])*100/128)
         loc = re.search(r'acc: [\d]*\.[\d]', st).span()
-        acc.append(float(st[loc[0]+5:loc[1]])*100)
+        acc.append(float(st[loc[0]+5:loc[1]]))
 loss = smooth(loss)
 acc = smooth(acc)
 
@@ -51,14 +41,14 @@ x = np.linspace(0, iters, iters)
 fig, ax1 = plt.subplots()
 ax2 = ax1.twinx()
 ax1.plot(x, loss, label='loss', color='r')
-ax2.plot(x, acc, label='acc', color='b')
-ax1.set_xlim(0000,)
+ax2.plot(x, acc, label='train_acc', color='b')
+ax1.set_xlim(0, iters)
 ax1.set_ylim(0., 8.)
-# ax2.set_ylim(40., 100.)
+ax2.set_ylim(0., 100.)
 ax1.set_ylabel('loss')
-ax2.set_ylabel('acc')
+ax2.set_ylabel('train_acc')
 plt.xlabel('iterations')
 plt.title("Loss/Acc Diagram")
-fig.legend(bbox_to_anchor=(0.3, 1), bbox_transform=ax1.transAxes)
+fig.legend(bbox_to_anchor=(1, 0.5), bbox_transform=ax1.transAxes)
 
 plt.show()
