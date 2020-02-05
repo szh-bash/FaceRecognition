86f7be872221d9aff71e2d950b22c18de7dbd5bc
Immortal.S <342333349@qq.com>
2020-02-05 14:09:12 +0800

    rename lfw_test.py

--- test/get_feat.py
+++ test/get_feat.py
@@ -1,14 +1,9 @@
 import os
 import re
 import sys
-import time
 import torch
-import torch.nn as nn
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
-# from progressbar import *
 import progressbar as pb
 
 sys.path.append("..")
@@ -29,17 +24,12 @@ def save_feat(ft, index, lim, path):
         ftx = ftx.tolist()
         pt = pd.DataFrame(data=ftx)
         pt.to_csv(path+name+'/'+str(idx), mode='w', index=None, header=None)
-        # print(path+name+'/'+str(idx), ftx)
 
 
 # load model
 model_path = '/data/shenzhonghai/FaceClustering/models/Vgg16_bs-128_lr-4|16k|19k_ep200.pt'
 device = torch.device('cuda:0')
 model = Vgg16().cuda()
-# check_point = torch.load(model_path)
-# if torch.cuda.device_count() > 1:
-#     model = nn.DataParallel(model, device_ids=[0, 1, 2, 3, 4, 5, 6])
-# model.load_state_dict(check_point)
 model.load_state_dict({k.replace('module.', ''): v for k, v in torch.load(model_path).items()})
 model.eval()  # DropOut/BN
 print(model)
@@ -60,49 +50,8 @@ widgets = [' ', pb.Percentage(),
 pgb = pb.ProgressBar(widgets=widgets, maxval=Total).start()
 for i, (inputs, labels) in enumerate(data_loader):
     feat = model(inputs.to(device))
-    # print("img %d done! %s" % (labels[0], data.name[ids]))
     save_feat(feat, ids, len(inputs), feat_path)
     ids += len(inputs)
     pgb.update(i)
 pgb.finish()
 print('Feature Map saved to \'%s\' successfully!' % feat_path[:-1])
-exit(0)
-
-
-def get_img_pairs_list(pairs_txt_path, img_path):
-    file = open(pairs_txt_path)
-    img_pairs_list, labels = [], []
-
-
-def plotROC(predStrengths, classLabels):
-    cur = (0.0, 0.0)
-    numPosClass = np.sum(np.array(classLabels) == 1.0)
-    yStep = 1.0/numPosClass
-    xStep = 1.0/(len(classLabels)-numPosClass)
-    print(np.array(predStrengths.flatten()))
-    sortedIndicies = np.argsort(-np.array(predStrengths.flatten()))
-    print(sortedIndicies)
-    fig = plt.figure()
-    fig.clf()
-    ySum = 0.0
-    ax = plt.subplot(111)
-    for index in sortedIndicies:
-        if classLabels[index] == 1.0:
-            delY = yStep; delX=0
-        else:
-            delY = 0; delX = xStep
-            ySum += cur[1]
-        ax.plot([cur[0], cur[0]+delX], [cur[1], cur[1]+delY], c='b')
-        cur = (cur[0]+delX, cur[1]+delY)
-        print(cur)
-    ax.plot([0, 1], [0, 1], 'b--')
-    ax.axis([0, 1, 0, 1])
-    plt.xlabel('False Positve Rate')
-    plt.ylabel('True Postive Rate')
-    plt.title('ROC curve for AdaBoost Horse Colic Detection System')
-    ax.axis([0, 1, 0, 1])
-    plt.show()
-    print('the Area under the curve is:', ySum*xStep)
-
-
-plotROC(100, [0,1])
