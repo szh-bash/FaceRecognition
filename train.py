db0dfe0cefcec40af576158db15d378acbb54df4
Immortal.S <342333349@qq.com>
2020-02-11 11:25:26 +0800

    wf_sm failed

--- train.py
+++ train.py
@@ -10,21 +10,10 @@ from loss import ArcMarginProduct as Arcface
 
 from init import DataReader
 
-save_path = '/data/shenzhonghai/FaceClustering/models/Vgg16_wf_af_128_1_ep'
-
-# set config
-data = DataReader('train', 'webface')
-batch_size = 128
-Total = 360
-learning_rate = 0.001
-
 
 def get_label(output):
     # print(output.shape)
     return torch.argmax(output, dim=1)
-
-
-grads = {}
 
 
 def save_grad(name):
@@ -34,7 +23,7 @@ def save_grad(name):
 
 
 def get_max_gradient(g):
-    print('Gradient:', g)
+    # print('Gradient:', g)
     pm = torch.max(g)
     nm = torch.min(g)
     if -nm > pm:
@@ -44,8 +33,16 @@ def get_max_gradient(g):
 
 
 if __name__ == '__main__':
+    # set config
+    save_path = '/data/shenzhonghai/FaceClustering/models/Vgg16_wf_sm_128_2_ep'
+    data = DataReader('train', 'webface')
+    batch_size = 128
+    Total = 360
+    learning_rate = 0.001
+    grads = {}
+
     # Some Args setting
-    net = Vgg16('train', 'arcface')
+    net = Vgg16('train', 'softmax', data.data_name)
     device = torch.device("cuda:0")
     if torch.cuda.device_count() > 1:
         devices_ids = [0, 1, 2, 3]
@@ -53,21 +50,26 @@ if __name__ == '__main__':
         print("Let's use %d/%d GPUs!" % (len(devices_ids), torch.cuda.device_count()))
     net.to(device)
     data_loader = DataLoader(dataset=data, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
-    optimizer = optim.Adam(net.parameters(), lr=learning_rate)
-    print(net.parameters())
     arcface = Arcface(4096, data.person).to(device)
     criterion = nn.CrossEntropyLoss().to(device)
+    optimizer = optim.Adam([{'params': net.parameters()},
+                            {'params': arcface.parameters()}],
+                           lr=learning_rate)
+    print(net.parameters())
+    print(arcface.parameters())
     torch.save(net.state_dict(), save_path+str(0)+'.pt')
     print('Model saved to %s' % (save_path + str(0) + '.pt'))
 
     num_params = 0
     for param in net.parameters():
         num_params += param.numel()
+    for param in arcface.parameters():
+        num_params += param.numel()
     print("Parameter Number: %d M" % (num_params / 1e6))
 
     print("Training Started!")
     iterations = 0
-    scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[100], gamma=0.1, last_epoch=-1)
+    scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[200], gamma=0.1, last_epoch=-1)
     for epoch in range(Total):
         data_time, train_time = 0, 0
         pred, train_x, train_y, loss = None, None, None, None
@@ -94,7 +96,7 @@ if __name__ == '__main__':
             train_time = train_time + tt
 
             iterations += 1
-            if iterations % 50 == 0:
+            if iterations % 200 == 0:
                 print('Output Max pred: %s' % torch.max(feat.gather(1, train_y.view(-1, 1))))
                 print('Abs Max Gradient of net_output:', get_max_gradient(grads['feat_grad'].gather(1, train_y.view(-1, 1))))
             if iterations % 1 == 0:
