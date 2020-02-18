cb0fc20265c062275bcf582b5f7d2977de9b8e05
Immortal.S <342333349@qq.com>
2020-02-18 22:50:48 +0800

    upd sh

---
+++ test/test.sh
@@ -0,0 +1,2 @@
+CUDA_VISIBLE_DEVICES=1 python get_feat.py
+python roc.py
---
+++ train.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES="7,2,3,5,6,1" python -u train.py | tee /data/shenzhonghai/FaceClustering/logs/train_log_Vgg16_wf_af-1_256_lr1e3_2\|60k.log
