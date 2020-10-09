6b996d06ed8356f2fda80ecc2d495e145b1c3c2c
Immortal.S <342333349@qq.com>
2020-10-09 15:33:47 +0800

    add 10-fold test

--- tester/roc.py
+++ tester/roc.py
@@ -46,32 +46,67 @@ def get_acc(threshold_list, cases, dist, ground_truth):
     return res
 
 
+def global_calc(_index, _ground_truth, __test_total):
+    _roc = 0
+    pos = 0
+    neg = 0
+    _max_test_acc = 0
+    __true_ratio = []
+    for idx in _index:
+        if _ground_truth[idx]:
+            pos += 1
+            if len(__true_ratio) > 0:
+                __true_ratio[-1] += 1 / __test_total * 2
+        else:
+            neg += 1
+            if len(__true_ratio) > 0:
+                _roc += pos * pow(1 / __test_total * 2, 2)
+            __true_ratio.append(pos / __test_total * 2)
+        _max_test_acc = max(_max_test_acc, (pos+__test_total//2-neg)/__test_total)
+    __true_ratio[-1] = 1.0
+    return __true_ratio, _max_test_acc*100, _roc
+
+
+def cross_acc(_dist, _ground_truth):
+    res = 0
+    for i in range(10):
+        p = _dist[i*600:(i+1)*600]
+        gt = _ground_truth[i*600:(i+1)*600]
+        _index = np.argsort(p)[::-1]
+        pos = 0
+        neg = 0
+        ma = 0
+        threshold = 0
+        for idx in _index:
+            if gt[idx]:
+                pos += 1
+            else:
+                neg += 1
+            if pos - neg >= ma:
+                ma = pos - neg
+                threshold = p[idx]
+        res += ((_dist > threshold) == _ground_truth).sum()
+    return res/10
+
+
 def calc(filepath):
     store, feats = get(filepath, data)
     _test_total, dist, ground_truth = get_img_pairs_list(**store)
     dist = np.array(dist)
+    index = np.argsort(dist)[::-1]
     ground_truth = np.array(ground_truth)
 
-    # Figure out test_acc
+    # Approximate test_acc
     _test_acc = get_acc(thresholds, _test_total, dist, ground_truth)
     print('Max test_acc: %.3f (threshold=%.5f)' % (_test_acc.max(), thresholds[_test_acc.argmax()]))
 
+    _cross_validation = cross_acc(dist, ground_truth) / _test_total * 100
+
     # Roc
-    index = np.argsort(dist)[::-1]
-    roc = 0
-    count = 0
-    _true_ratio = []
-    for idx in index:
-        if ground_truth[idx]:
-            count += 1
-            if len(_true_ratio) > 0:
-                _true_ratio[-1] += 1 / _test_total * 2
-        else:
-            if len(_true_ratio) > 0:
-                roc += count * pow(1 / _test_total * 2, 2)
-            _true_ratio.append(count / _test_total * 2)
-    _true_ratio[-1] = 1.0
+    _true_ratio, max_test_acc, roc = global_calc(index, ground_truth, _test_total)
     eer = np.abs(1 - np.array(_true_ratio) - np.linspace(0, 1.0, (_test_total // 2)))
+    print('Global Test Accuracy: %.3f ' % max_test_acc)
+    print('Cross-validation Test Accuracy: %.3f' % _cross_validation)
     print('@FAR = 0.00100: TAR = %.5f' % _true_ratio[2])
     print('@FAR = 0.01000: TAR = %.5f' % _true_ratio[29])
     print('@FAR = 0.02000: TAR = %.5f' % _true_ratio[58])
