49e3c0fc3772592c8ca6ac3d5415ddb1162ce41d
Immortal.S <342333349@qq.com>
2021-05-08 15:01:29 +0800

    try border0

--- test/test.sh
+++ test/test.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=4 python -u verification.py | tee /data/shenzhonghai/FaceClustering/logs/test_log_resnetFace50_RetinaWeb112FC_m45.log
+CUDA_VISIBLE_DEVICES=9 python -u verification.py | tee /data/shenzhonghai/FaceClustering/logs/test_log_resnetFace50_RetinaWeb112FC_m40_border0_cutout.log
